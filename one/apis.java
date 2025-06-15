7018e2805d777b4732a1b86a4672bc421ce83ea3
Shubham soni <shubhamsoni98766@gmai.com>
2025-06-15 07:52:43 +0530

    more apis addes

--- one/apis.java
+++ one/apis.java
@@ -4,5 +4,7 @@ public class apis {
     public static void main(String[] args) {
 
         System.out.println("here we can add out apis" );
+        System.out.println("added more apis ");
+        System.out.println("one external api");
     }
 }
